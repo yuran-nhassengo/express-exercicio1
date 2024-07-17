a234696c44d286aa23d36366b704206e0f22e384
yuran-nhassengo <yurannhassengo@gmail.com>
2024-07-18 00:53:42 +0300

    add body

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+
 const port = 3030;
 
 const app = express()
 
 app.use(cors());
+app.use(express.json())
 
-const users = [
+let users = [
     {
     nome:'Yuran',
     contacto:"849102433",
@@ -24,6 +26,13 @@ app.get('/',(req,res) =>{
     res.type('application/json');
     res.send(users);
 });
+
+app.post('/user',(req,res) =>{
+    const {n,c,m} = req.body;
+    let newUser = {n,c,m}
+    users.push(newUser);
+    res.json({mm:"Usuario adicionando com sucesso",n});
+})
 
 app.listen(port,()=>{
     console.log('Servidor iniciado');
